b959f54f994f03eff3ccfcc3782457d2f70bcef2
Devaharish M <102893165+devaharish2004@users.noreply.github.com>
2023-07-08 22:26:45 +0530

    Update Code.cpp

--- BS on answers/Find Kth Missing Positive Number/Code.cpp
+++ BS on answers/Find Kth Missing Positive Number/Code.cpp
@@ -1,5 +1,6 @@
 int missingK(vector < int > vec, int n, int k) {
     // Write your code here.
+    //check the notes or youtube video for more detailed explanation
     int low = 0;
     int high = n-1;
     while(low <= high)
