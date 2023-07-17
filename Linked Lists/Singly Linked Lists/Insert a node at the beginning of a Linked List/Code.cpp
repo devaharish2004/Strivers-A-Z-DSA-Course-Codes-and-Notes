67440346f1914df9efb08f481a77142b16109a5a
Devaharish M <102893165+devaharish2004@users.noreply.github.com>
2023-07-17 23:03:55 +0530

    Add files via upload

---
+++ Linked Lists/Singly Linked Lists/Insert a node at the beginning of a Linked List/Code.cpp
@@ -0,0 +1,19 @@
+/**
+ * Definition of linked list
+ * class Node {
+ *
+ * public:
+ *     int data;
+ *     Node* next;
+ *     Node() : data(0), next(nullptr) {}
+ *     Node(int x) : data(x), next(nullptr) {}
+ *     Node(int x, Node* next) : data(x), next(next) {}
+ * };
+ */
+
+Node* insertAtFirst(Node* list, int newValue) {
+    // Write your code here
+    Node* newNode = new Node(newValue,list);
+    list = newNode;
+    return(list);
+}
