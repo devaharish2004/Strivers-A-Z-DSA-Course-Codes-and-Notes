50bd91eefe5cd2ac917beff444c951390904cfb0
Devaharish M <102893165+devaharish2004@users.noreply.github.com>
2023-08-08 23:04:26 +0530

    Add files via upload

--- Linked Lists/Medium Problems on LL/Odd Even LL/Code.cpp
+++ Linked Lists/Medium Problems on LL/Odd Even LL/Code.cpp
@@ -1,20 +1,68 @@
-//Problem 1 - Leetcode Variant
-class Solution {
-public:
-    ListNode* oddEvenList(ListNode* head) {
-        if(!head || !head->next || !head->next->next) return head;
-        
-        ListNode *odd = head;
-        ListNode *even = head->next;
-        ListNode *even_start = head->next;
-        
-        while(odd->next && even->next){
-            odd->next = even->next; //Connect all odds
-            even->next = odd->next->next;  //Connect all evens
-            odd = odd->next;
-            even = even->next;
+/*
+    Time complexity: O(N)
+    Space complexity: O(N)
+    
+    Where 'N' is number of nodes in linked list.
+*/
+
+Node* segregateEvenOdd(Node* head)
+{
+    // Create two different linked list 'even' and 'odd'
+    Node *oddStart = NULL;
+    Node *oddEnd = NULL;
+    Node *evenStart = NULL;
+    Node *evenEnd = NULL;
+    
+    // Copy the 'head'
+    Node *curr = head; 
+
+    while(curr != NULL)
+    {
+        int currVal = curr -> data;
+
+        // Current node value is even then add in even linked list
+        if(currVal % 2 == 0)
+        {
+            // If current node is first node of even linked list
+            if(evenStart == NULL)
+            {
+                evenStart = new Node(currVal);
+                evenEnd = evenStart;
+            }
+            
+            else
+            {
+                evenEnd -> next = curr;
+                evenEnd = evenEnd -> next;
+            }
         }
-        odd->next = even_start;   //Place the first even node after the last odd node.
-        return head; 
+        
+        // Current node value is odd then add in odd linked list
+        else
+        {
+            // If current node is first node of odd linked list
+            if(oddStart == NULL)
+            {
+                oddStart = new Node(currVal);
+                oddEnd = oddStart;
+            }
+            else
+            {
+                oddEnd -> next = curr;
+                oddEnd = oddEnd -> next;
+            }
+        }
+        
+        curr = curr -> next;
     }
-};
+
+    // At least one even and odd node present in linked list
+    if(oddStart!= NULL and evenStart != NULL)
+    {
+        evenEnd -> next = oddStart;
+        oddEnd -> next = NULL;
+        head = evenStart;
+    }
+        
+    return head;
+}
