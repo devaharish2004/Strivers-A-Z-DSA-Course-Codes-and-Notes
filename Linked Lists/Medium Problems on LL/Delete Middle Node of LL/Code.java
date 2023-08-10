66e59ec08db7aeb596d0618d6ff09b281e929f30
devaharish2004 <devaharishm@gmail.com>
2023-08-10 22:49:12 +0530

    Create code and notes

---
+++ Linked Lists/Medium Problems on LL/Delete Middle Node of LL/Code.java
@@ -0,0 +1,49 @@
+/****************************************************************
+
+ Following is the class structure of the Node class:
+
+ class Node {
+     public int data;
+     public Node next;
+
+     Node()
+     {
+         this.data = 0;
+         this.next = null;
+     }
+
+     Node(int data)
+     {
+         this.data = data;
+         this.next = null;
+     }
+
+     Node(int data, Node next)
+     {
+         this.data = data;
+         this.next = next;
+     }
+ };
+
+ *****************************************************************/
+
+public class Code {
+    public static Node deleteMiddle(Node head) {
+        // Write your code here.
+        if(head == null || head.next == null)
+        {
+            return null;
+        }
+        Node dummy = new Node(0,head);
+        Node slow = dummy;
+        Node fast = dummy;
+
+        while(fast.next != null && fast.next.next != null)
+        {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        slow.next = slow.next.next;
+        return dummy.next;
+    }
+}
