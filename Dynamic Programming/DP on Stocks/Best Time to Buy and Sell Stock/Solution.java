d2968830ee02ee18a58832b72a28472fa94cb8fe
devaharish2004 <devaharishm@gmail.com>
2024-01-31 08:31:06 +0530

    Buy and Sell stock

---
+++ Dynamic Programming/DP on Stocks/Best Time to Buy and Sell Stock/Solution.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int mini = Integer.MAX_VALUE;
+        int maxprofit = 0;
+        for(int i = 0; i < prices.length; i++)
+        {
+            int cost = prices[i] - mini;
+            maxprofit = Math.max(maxprofit, cost);
+            mini = Math.min(mini, prices[i]);
+        }
+
+        return maxprofit;
+    }
+}
